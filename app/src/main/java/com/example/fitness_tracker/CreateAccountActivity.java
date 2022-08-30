36fe3fe90f902f27d3d16282d65d895c7630777e
Kristaps Kozulis <kristaps1000@gmail.com>
2022-08-30 18:33:26 +0300

    App successfully registers users to Firebase database

--- app/src/main/java/com/example/fitness_tracker/CreateAccountActivity.java
+++ app/src/main/java/com/example/fitness_tracker/CreateAccountActivity.java
@@ -1,14 +1,110 @@
 package com.example.fitness_tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText usernameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountButton;
+    TextView loginButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        usernameEditText = findViewById(R.id.username_edit_text);
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+
+        createAccountButton = findViewById(R.id.submit_sign_up_button);
+
+        loginButton = findViewById(R.id.login_text_view_button);
+
+        progressBar = findViewById(R.id.progress_c);
+
+        createAccountButton.setOnClickListener(v -> createAccount());
+        loginButton.setOnClickListener(v -> finish());
+    }
+
+    void createAccount() {
+        String username = usernameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValid = validateData(username, email, password, confirmPassword);
+        if (!isValid) return;
+
+        createAccountInFirebase(username, email, password);
+    }
+
+    boolean validateData(String username, String email, String password, String confirmPassword) {
+        // Validate user input
+        if (username.length() < 4) {
+            usernameEditText.setError("Username must be at least 4 characters");
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if (password.length() < 7) {
+            passwordEditText.setError("Password must be at least 7 characters");
+            return false;
+        }
+        if (!password.equals(confirmPassword)) {
+            confirmPasswordEditText.setError("Passwords doesn't match");
+            confirmPasswordEditText.setText("");
+            return false;
+        }
+        return true;
+    }
+
+    void createAccountInFirebase(String username, String email, String password) {
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccountActivity.this,
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(CreateAccountActivity.this, "Successfully created account", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.getCurrentUser().sendEmailVerification();
+                    firebaseAuth.signOut();
+                    finish();
+                } else {
+                    Toast.makeText(CreateAccountActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void changeInProgress(boolean inProgress) {
+        if (inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountButton.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            createAccountButton.setVisibility(View.VISIBLE);
+        }
     }
 }
